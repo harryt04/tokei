574225a93698af57adf51fcd01f4ff4d19a977a6
Harry Thomas <hsthomas@verisk.com>
2024-08-08 18:07:49 -0600

    shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <span className="text-6xl">Welcome to tokei</span>
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center gap-4 py-20 px-10 min-h-screen",
+        )}>
+        <div className="text-6xl">Welcome to tokei</div>
+        <div></div>
+        <br />
+        <Button variant="outline" color="accent">
+          shadcn button test
+        </Button>
       </div>
     </>
   );
