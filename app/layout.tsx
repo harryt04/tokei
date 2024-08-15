e3e252c4aaee104d03f84f73d3412d1720aa136a
Harry Thomas <hsthomas@verisk.com>
2024-08-14 22:43:35 -0600

    updated description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,9 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "tokei",
-  description: "Sequential timers for power users",
+  title: "Tokei",
+  description:
+    "Tokei is the ultimate timer app for power users, designed for chefs in the kitchen, fitness instructors, and project managers who need to set precise, sequential routines to keep everything on track.",
 };
 
 export default function RootLayout({
