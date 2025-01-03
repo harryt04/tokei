612a98e4ac1b193385258b01aa12bb304bb1fd9c
Harry Thomas <hsthomas@verisk.com>
2025-01-03 09:10:22 -0700

    fix build

--- app/routine/[id]/page.tsx
+++ app/routine/[id]/page.tsx
@@ -1,11 +1,10 @@
-'use client'
-
-import { Routine } from '@/models'
-import { useParams } from 'next/navigation'
-import { useQuery } from '@tanstack/react-query'
-
-export default function RoutinePage() {
-  const { id } = useParams() // Access the [id] from the route
+export default async function RoutinePage(props: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await props.params
+  const response = await fetch(`/api/routine?id=${id}`)
+  console.log('response: ', response)
+  console.log('id: ', id)
   return (
     <div>
       <h1>Routine ID: {id}</h1>
