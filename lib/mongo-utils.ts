1dd20ca6fd9509279f3c791517f088e3e6c62a32
Harry Thomas <hsthomas@verisk.com>
2025-03-12 18:21:36 -0600

    build fix; mongo init

---
+++ lib/mongo-utils.ts
@@ -0,0 +1,30 @@
+// import { getMongoClient, mongoDBConfig } from './mongo-client'
+
+/**
+ * Creates necessary indexes for the MongoDB collections.
+ */
+export async function createMongoDbIndexes(dbName: string) {
+  console.log(`Ensuring indexes for db: ${dbName} ...`)
+  // const client = await getMongoClient()
+  // const db = client.db(dbName)
+
+  // const coll = mongoDBConfig.collections
+
+  // const collectionNames = (await db.listCollections().toArray()).map(
+  //   (c) => c.name,
+  // )
+
+  // async function ensureIndex(collectionName: string, indexes: any[]) {
+  //   if (!collectionNames.includes(collectionName)) {
+  //     console.log(
+  //       `Collection "${collectionName}" does not exist. Creating it...`,
+  //     )
+  //     await db.createCollection(collectionName)
+  //   }
+  //   await db.collection(collectionName).createIndexes(indexes)
+  // }
+
+  // await ensureIndex(coll., [{ key: { blahId: 1 } }])
+
+  console.log('Indexes ensured successfully for db: ', dbName)
+}
