41f5acb9192eb8c14ea5d4076db2f2d8c2239758
Harry Thomas <hsthomas@verisk.com>
2025-03-21 18:33:16 -0600

    better mobile padding and spacing

--- app/freestyle/page.tsx
+++ app/freestyle/page.tsx
@@ -9,8 +9,10 @@ export default function Freestyle() {
         <RedirectToSignIn />
       </SignedOut>
       <SignedIn>
-        <MySidebarTrigger />
-        <FreestyleList />
+        <div className="w-full">
+          <MySidebarTrigger />
+          <FreestyleList />
+        </div>
       </SignedIn>
     </>
   )
