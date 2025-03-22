3e8e9056f159982960c4f549ea29ddb8ae8ec6f2
Harry Thomas <hsthomas@verisk.com>
2025-03-21 19:51:45 -0600

    implemented dnd

--- components/custom/freestyle-list.tsx
+++ components/custom/freestyle-list.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import FreestyleComponent from './freestyle-component'
 import { ClockIcon, PlusIcon } from 'lucide-react'
 import { Button } from '../ui/button'
@@ -50,59 +51,93 @@ function FreestyleList() {
     backupTimers(timers)
   }, [timers])
 
+  const onDragEnd = (result: any) => {
+    if (!result.destination) return
+
+    const reorderedTimers = Array.from(timers)
+    const [removed] = reorderedTimers.splice(result.source.index, 1)
+    reorderedTimers.splice(result.destination.index, 0, removed)
+
+    setTimers(reorderedTimers)
+  }
+
   return (
-    <div className="flex w-full flex-col gap-4 p-4">
-      {timers.map((timer, index) => (
-        <FreestyleComponent
-          key={timer.id}
-          timer={timer}
-          timerUpdated={(newTimer) => {
-            setTimers((prevTimers) => {
-              const updatedTimers = [...prevTimers]
-              updatedTimers[index] = { ...newTimer }
-              return updatedTimers
-            })
-          }}
-          removeTimer={(id) => {
-            setTimers((prevTimers) => {
-              const newTimerList = prevTimers.filter((t) => t.id !== id)
-              if (newTimerList.length === 0) backupTimers([])
-              return newTimerList
-            })
-          }}
-        />
-      ))}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="flex w-full flex-col gap-4 p-4">
+        <Droppable droppableId="timers">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="flex flex-col gap-4"
+            >
+              {timers.map((timer, index) => (
+                <Draggable key={timer.id} draggableId={timer.id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <FreestyleComponent
+                        timer={timer}
+                        timerUpdated={(newTimer) => {
+                          setTimers((prevTimers) => {
+                            const updatedTimers = [...prevTimers]
+                            updatedTimers[index] = { ...newTimer }
+                            return updatedTimers
+                          })
+                        }}
+                        removeTimer={(id) => {
+                          setTimers((prevTimers) => {
+                            const newTimerList = prevTimers.filter(
+                              (t) => t.id !== id,
+                            )
+                            if (newTimerList.length === 0) backupTimers([])
+                            return newTimerList
+                          })
+                        }}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
 
-      {timers.length === 0 && (
-        <>
-          <NoResultsComponent
-            icon={<ClockIcon />}
-            title="No timers"
-            body={[
-              'Click the button below to get started.',
-              'Create as many timers as you need.',
-            ]}
-          />
-        </>
-      )}
+        {timers.length === 0 && (
+          <>
+            <NoResultsComponent
+              icon={<ClockIcon />}
+              title="No timers"
+              body={[
+                'Click the button below to get started.',
+                'Create as many timers as you need.',
+              ]}
+            />
+          </>
+        )}
 
-      <Button
-        variant="secondary"
-        onClick={() => {
-          setTimers((previousTimers) => [
-            ...previousTimers,
-            {
-              id: uuidv4(),
-              name: `Timer ${previousTimers.length + 1}`,
-              duration: 0,
-            },
-          ])
-        }}
-      >
-        <PlusIcon />
-        Add timer
-      </Button>
-    </div>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            setTimers((previousTimers) => [
+              ...previousTimers,
+              {
+                id: uuidv4(),
+                name: `Timer ${previousTimers.length + 1}`,
+                duration: 0,
+              },
+            ])
+          }}
+        >
+          <PlusIcon />
+          Add timer
+        </Button>
+      </div>
+    </DragDropContext>
   )
 }
 
