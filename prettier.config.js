e27fdfa3b5076fc9ddc90960eb8b7ab7203bf919
Harry Thomas <hsthomas@verisk.com>
2025-03-29 11:51:11 -0600

    revert prettier config change

--- prettier.config.js
+++ prettier.config.js
@@ -4,8 +4,4 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   plugins: ['prettier-plugin-tailwindcss'],
-  // put jsx ending on same line when possible
-  bracketSameLine: true,
-  // enforce self-closing tags where possible
-  jsxSelfClosing: true,
 }
