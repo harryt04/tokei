28fa0ab46c3d001ec66e883e98aba4dfac445803
Harry Thomas <hsthomas@verisk.com>
2025-12-30 09:39:43 -0700

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,14 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: 'Tokei 時計',
   description: `Tokei (時計, meaning 'clock' in Japanese) is a free, open-source timer app built for power users. Designed specifically for chefs in the kitchen, it helps manage multiple timers simultaneously, ensuring all dishes are finished at the perfect moment.`,
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
